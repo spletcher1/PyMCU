b728d13459c03032fb3cc8ccc774bec701793fd9
Scott Pletcher <spletch@umich.edu>
2020-05-29 01:10:55 -0400

    Minor fix

--- Message.py
+++ Message.py
@@ -10,8 +10,8 @@ class Message():
         self.messageNumber=number
     
     def __str__(self):
-        ss="(" + str(self.messageNumber)+")  "+self.timeOfError.strftime("%m/%d/%Y %H:%M:%S  ")
-        ss+="-> "+str(self.DFMId)+" <-  "+self.message 
+        ss="(" + str(self.messageNumber)+")  "+self.timeOfError.strftime("%m/%d/%Y %H:%M:%S ")
+        ss+="-> "+str(self.DFMId)+" <- "+self.message 
         if(self.messageType==Enums.MESSAGETYPE.ERROR):
             ss+=" (Error)"
         elif(self.messageType==Enums.MESSAGETYPE.NOTICE):
