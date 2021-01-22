45b7c5fe3b01514b70b8d849a3af0093db1f0bb2
Scott Pletcher <spletch@umich.edu>
2021-01-22 12:56:56 -0500

    Again some adjustments to envmonitor.

--- TSL2591.py
+++ TSL2591.py
@@ -12,6 +12,8 @@ class TSL2591:
             self.theSensor.gain = adafruit_tsl2591.GAIN_MED 
             # Set integration time (intervals of 100ms)
             self.theSensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_300MS
+            self.isAtMaxSensitivity = False
+            self.isAtMinSensitivity = False
     def Set100msIntegrationTime(self):
         self.theSensor.integration_time=adafruit_tsl2591.INTEGRATIONTIME_100MS
     def Set200msIntegrationTime(self):
@@ -47,38 +49,47 @@ class TSL2591:
             else :
                 print("Adjusting sensitivity...")
 
+
+    def ValidateTimeAndGain(self):
+        isOkay=True
+        tmp_time = self.theSensor.integration_time
+        tmp_gain = self.theSensor.gain
+
+        if(tmp_time!= self.theSensor._integration_time):
+            self.theSensor.integration_time = self.theSensor._integration_time  
+            isOkay=False                      
+
+        if(tmp_gain!= self.theSensor._gain):
+            self.theSensor.gain = self.theSensor._gain           
+            isOkay=False
+        
+        return isOkay
+
     def IncreaseSensitivity(self):       
         wasSensitivityIncreased=True
-        if self.theSensor.gain == adafruit_tsl2591.GAIN_LOW :
+        if self.theSensor.integration_time == adafruit_tsl2591.INTEGRATIONTIME_100MS :
+            self.theSensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_300MS
+        elif self.theSensor.integration_time == adafruit_tsl2591.INTEGRATIONTIME_300MS :
+            self.theSensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_500MS
+        elif self.theSensor.gain == adafruit_tsl2591.GAIN_LOW :
             self.theSensor.gain = adafruit_tsl2591.GAIN_MED
         elif self.theSensor.gain == adafruit_tsl2591.GAIN_MED :
             self.theSensor.gain = adafruit_tsl2591.GAIN_HIGH
-        elif self.theSensor.integration_time == adafruit_tsl2591.INTEGRATIONTIME_100MS :
-            self.theSensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_200MS
-        elif self.theSensor.integration_time == adafruit_tsl2591.INTEGRATIONTIME_200MS :
-            self.theSensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_300MS
-        elif self.theSensor.integration_time == adafruit_tsl2591.INTEGRATIONTIME_300MS :
-            self.theSensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_400MS
-        elif self.theSensor.integration_time == adafruit_tsl2591.INTEGRATIONTIME_400MS :
-            self.theSensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_500MS
         else :
             wasSensitivityIncreased=False
+
         return wasSensitivityIncreased
 
     def DecreaseSensitivity(self):        
         wasSensitivityDecreased=True      
-        if self.theSensor.integration_time == adafruit_tsl2591.INTEGRATIONTIME_500MS :
-            self.theSensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_400MS
-        elif self.theSensor.integration_time == adafruit_tsl2591.INTEGRATIONTIME_400MS :
-            self.theSensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_300MS
-        elif self.theSensor.integration_time == adafruit_tsl2591.INTEGRATIONTIME_300MS :
-            self.theSensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_200MS
-        elif self.theSensor.integration_time == adafruit_tsl2591.INTEGRATIONTIME_200MS :
-            self.theSensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_100MS
-        elif self.theSensor.gain == adafruit_tsl2591.GAIN_HIGH :
+        if self.theSensor.gain == adafruit_tsl2591.GAIN_HIGH :
             self.theSensor.gain = adafruit_tsl2591.GAIN_MED
         elif self.theSensor.gain == adafruit_tsl2591.GAIN_MED :
             self.theSensor.gain = adafruit_tsl2591.GAIN_LOW
+        elif self.theSensor.integration_time == adafruit_tsl2591.INTEGRATIONTIME_500MS :
+            self.theSensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_300MS
+        elif self.theSensor.integration_time == adafruit_tsl2591.INTEGRATIONTIME_300MS :
+            self.theSensor.integration_time = adafruit_tsl2591.INTEGRATIONTIME_100MS
         else:
             wasSensitivityDecreased=False
         return wasSensitivityDecreased
@@ -87,6 +98,8 @@ class TSL2591:
         if self.theSensor is None:
             return -1
         try:
+            if (self.ValidateTimeAndGain()==False):
+                return -99
             lux = self.theSensor.lux
             # returning -99 will ensure the state machine does not move on
             # and will call light again next step.
