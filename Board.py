9dfd84f624651b24a5703c6645fb4ed91a972498
Scott Pletcher <spletch@umich.edu>
2024-09-13 11:32:11 -0400

    First full draft of pulser incorporation.

--- Board.py
+++ Board.py
@@ -7,6 +7,10 @@ if("MCU" in platform.node()):
 class BoardSetup():
     def __init__(self):        
 
+    
+
+        self.output0pin=19
+        self.output1pin=18
         self.ledPIN=13
         self.relayPIN=6
         self.boardIDPin=5
@@ -16,6 +20,25 @@ class BoardSetup():
         self.BoardID = InputDevice(pin=self.boardIDPin, pull_up=False)
         
         self.TurnOnDFMPower()
+        self.isPulsing=0
+
+        GPIO.setup(self.ledPIN,GPIO.OUT)
+        GPIO.setup(self.relayPIN,GPIO.OUT)
+        GPIO.output(self.relayPIN,GPIO.HIGH)
+        GPIO.output(self.ledPIN,GPIO.HIGH) 
+    
+        GPIO.setup(self.boardIDPin,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+        GPIO.setup(self.output0pin,GPIO.OUT)
+        GPIO.setup(self.output1pin,GPIO.OUT)
+        self.output0Frequency=40
+        self.output0DC=32
+        self.output1Frequency=40
+        self.output1DC=32
+
+        self.pwm0 = GPIO.PWM(self.output0pin,self.output0Frequency)        
+        self.pwm1 = GPIO.PWM(self.output1pin,self.output1Frequency)
+        
 
     def IsDFMV3Board(self):
         if (self.GetBoardVersion()=="V3"):
@@ -35,6 +58,24 @@ class BoardSetup():
         else:
             return "V2"
 
+    def StartPulsing(self,lights):
+        if(self.isPulsing==3):
+            return
+        if(lights==3):
+            self.pwm0.start(self.output0DC)
+            self.pwm1.start(self.output1DC)
+            self.pwm0.ChangeFrequency(self.output0Frequency)
+            self.pwm1.ChangeFrequency(self.output1Frequency)            
+        elif(lights==2):
+            self.pwm0.stop()            
+            self.pwm1.start(self.output1DC)
+            self.pwm1.ChangeFrequency(self.output1Frequency)
+        elif(lights==1):
+            self.pwm0.start(self.output0DC)
+            self.pwm0.ChangeFrequency(self.output0Frequency)
+            self.pwm1.stop()
+        self.isPulsing=lights
+        
     def TurnOnDFMPower(self):
         self.relay.on()
         self.Led.on()       
