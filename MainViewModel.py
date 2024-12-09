1481c8e360b126acb67d05326ce5402038e768fe
Scott Pletcher <spletch@umich.edu>
2024-12-08 21:50:06 -0500

    isc dark theme stuff

--- MainViewModel.py
+++ MainViewModel.py
@@ -974,16 +974,33 @@ def main2():
     files.sort(reverse=True)   
     print(files)
 
+def set_dark_theme(app):
+    palette = QPalette()
+    palette.setColor(QPalette.ColorRole.Window, QColor(53, 53, 53))
+    palette.setColor(QPalette.ColorRole.WindowText, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.Base, QColor(25, 25, 25))
+    palette.setColor(QPalette.ColorRole.AlternateBase, QColor(53, 53, 53))
+    palette.setColor(QPalette.ColorRole.ToolTipBase, Qt.GlobalColor.black)
+    palette.setColor(QPalette.ColorRole.ToolTipText, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.Text, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.Button, QColor(53, 53, 53))
+    palette.setColor(QPalette.ColorRole.ButtonText, Qt.GlobalColor.white)
+    palette.setColor(QPalette.ColorRole.BrightText, Qt.GlobalColor.red)
+    palette.setColor(QPalette.ColorRole.Link, QColor(42, 130, 218))
+    palette.setColor(QPalette.ColorRole.Highlight, QColor(42, 130, 218))
+    palette.setColor(QPalette.ColorRole.HighlightedText, Qt.GlobalColor.black)
+    app.setPalette(palette)
+
 def main():
     if("MCU" in platform.node()):
         theBoard=Board.BoardSetup()  
     else:
         theBoard=Board.BoardMock()
     app = QtWidgets.QApplication(sys.argv)    
+    set_dark_theme(app)
     myapp = MyMainWindow(theBoard)    
     #ModuleTest()    
     myapp.showFullScreen()
-    #myapp.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
     #myapp.showMaximized()
 
     sys.exit(app.exec()) 
